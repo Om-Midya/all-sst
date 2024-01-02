6c29040a24cc4fde54aa92681d7cacff9e7ada15
Kushagra S <kali@kali.teamkushagra.tech>
2024-01-02 23:36:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import Initial from './screens/Initial';
+import Login from './screens/Login';
+
+function App() {
+
+  var userLoggedIn = false;
+
+  if(userLoggedIn) {
+    return <Initial />;
+  }
+  else {
+    return  <Login />;
+  }
+
+}
+
+export default App;
